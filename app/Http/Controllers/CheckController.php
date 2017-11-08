ce001b6c7711a2ea18329f271786db392f34b7bf
munene02 <munene02@gmail.com>
2017-11-08 18:54:34 +0300

    stkpush

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -33,7 +33,7 @@ class CheckController extends Controller
 
 		$stkPushSimulation = $mpesa->STKPushSimulation($BusinessShortCode, $LipaNaMpesaPasskey, $TransactionType, $Amount, $PartyA, $PartyB, $PhoneNumber, $CallBackURL, $AccountReference, $TransactionDesc, $Remarks);
 
-		$checkoutRequestID = $stkPushSimulation['CheckoutRequestID'];
+		$checkoutRequestID = $stkPushSimulation->CheckoutRequestID;
 		// $timestamp='20'.date("ymdhis");
   //       $password=base64_encode('password');
 
@@ -44,7 +44,7 @@ class CheckController extends Controller
 		// 	}
 		//$decode = json_decode($stkPushSimulation);
         //return $stkPushSimulation;
-        return $stkPushSimulation.'<==>'.$checkoutRequestID;
+        return $stkPushSimulation;
 		
 
 
