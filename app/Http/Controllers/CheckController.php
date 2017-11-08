db737a85987add1a7687b0f8f126ecabc3717818
munene02 <munene02@gmail.com>
2017-11-08 11:18:48 +0300

    password

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -29,7 +29,7 @@ class CheckController extends Controller
 
 		// $balanceInquiry=$mpesa->accountBalance($CommandID, $Initiator, $SecurityCredential, $PartyA, $IdentifierType, $Remarks, $QueueTimeOutURL, $ResultURL);
 
-		$BusinessShortCode = "601340";
+		$BusinessShortCode = "174379";
 		$LipaNaMpesaPasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 		$TransactionType = "CustomerPayBillOnline";
 		$Amount = "1";
