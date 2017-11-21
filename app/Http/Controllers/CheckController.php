c12323756e7ad15cbd34809f076e44cbc0a39ae9
munene02 <munene02@gmail.com>
2017-11-21 16:41:18 +0300

    add

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -19,19 +19,19 @@ class CheckController extends Controller
 
 		$b2bTransaction=$mpesa->confirm();
 
-		// $BusinessShortCode = "174379";
-		// $LipaNaMpesaPasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-		// $TransactionType = "CustomerPayBillOnline";
-		// $Amount = "1";
-		// $PartyA = "254713467781";
-		// $PartyB = "174379";
-		// $PhoneNumber = "254713467781";
-		// $CallBackURL = "http://crysrockeng.com/call";
-		// $AccountReference = "350 Daily Plan";//plans
-		// $TransactionDesc = "test";
-		// $Remarks = "Remarked";
+		$BusinessShortCode = "174379";
+		$LipaNaMpesaPasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+		$TransactionType = "CustomerPayBillOnline";
+		$Amount = "1";
+		$PartyA = "254713467781";
+		$PartyB = "174379";
+		$PhoneNumber = "254713467781";
+		$CallBackURL = "http://crysrockeng.com/call";
+		$AccountReference = "350 Daily Plan";//plans
+		$TransactionDesc = "test";
+		$Remarks = "Remarked";
 
-		// $stkPushSimulation = $mpesa->STKPushSimulation($BusinessShortCode, $LipaNaMpesaPasskey, $TransactionType, $Amount, $PartyA, $PartyB, $PhoneNumber, $CallBackURL, $AccountReference, $TransactionDesc, $Remarks);
+		$stkPushSimulation = $mpesa->STKPushSimulation($BusinessShortCode, $LipaNaMpesaPasskey, $TransactionType, $Amount, $PartyA, $PartyB, $PhoneNumber, $CallBackURL, $AccountReference, $TransactionDesc, $Remarks);
 
 		// //$id = $stkPushSimulation.CheckoutRequestID;
 		// // $timestamp='20'.date("ymdhis");
@@ -44,10 +44,10 @@ class CheckController extends Controller
 		// // 	}
 		// $decode = json_decode($stkPushSimulation, true);
 		// $id = $stkPushSimulation[0];
-  //       return $stkPushSimulation;
+        return $stkPushSimulation;
   //       return $id;
 
-        var_dump($decode);
+        //var_dump($decode);
         //var_dump($stkPushSimulation);
 		
 
